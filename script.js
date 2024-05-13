460451bfcfe6867e4c791e58b9450397aa4f33ea
mrezwan <r.rajib0@gmail.com>
2024-05-13 19:56:44 +0600

    Initail commit

---
+++ script.js
@@ -0,0 +1,84 @@
+// let myString = "hello world";
+// console.log(myString.toUpperCase(charAt(0)));
+
+/*let myStr = "hello world";
+
+let myNstr;
+
+myNstr = `${myStr[0].toUpperCase()}${myStr.substring(1)}`;
+console.log(myNstr);
+
+myNstr = myStr.charAt(0).toUpperCase() + myStr.substring(1);
+console.log(myNstr);
+
+let x;
+let num = new Number(1326574255444);
+
+x = num.toString().length;
+x = num.toFixed(2);
+
+console.log(x); */
+
+/* let x;
+
+x = Math.random() * 100 + 1;
+console.log(x);
+
+x = 100;
+let y;
+
+y = 23;
+
+let outPut = x + y;
+console.log(outPut);
+let differenceBtwn = x - y;
+console.log(differenceBtwn);
+
+let rmdr = x / y;
+console.log(rmdr);
+
+let product = x * y;
+console.log(product);
+
+let mod = x % y;
+
+console.log(mod);
+
+let d;
+
+d = new Date();
+
+console.log(d); */
+
+//Array
+
+/*let fruits = ["Guava", "Apple", "Orange", "Mango", "Dates"];
+
+let Mango = ["Himsagor", "Langra", "Harivanga"];
+
+// fruits.push(Mango);
+
+let x = [...fruits, ...Mango];
+
+console.log(x);*/
+
+/*let x = Math.round(Math.random() * 100);
+
+let y = Math.round(Math.random() * 50);
+
+console.log(x);
+
+console.log(y);*/
+
+/*let myArr = [1, 3, 5, 7, 10, 85, 23];
+
+let z = myArr.sort((a, b) => a - b);
+
+console.log(z.reverse());*/
+
+let arr1 = [1, 2, 3, 4, 5];
+let arr2 = [5, 6, 7, 8, 9, 10];
+
+let arr3 = arr1.slice(0, 4).concat(arr2);
+
+console.log(arr3);
